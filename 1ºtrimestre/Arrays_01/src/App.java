6e0c7c4eff0baf121d125c9411c450c13af67766
rgs0042 <rgs0042@alu.medac.es>
2025-10-07 16:05:40 +0200

    Add files via upload

---
+++ 1ºtrimestre/Arrays_01/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int [] holas = {1,2,3,4};
+        for(int i = 0; i < holas.length();i++){
+            System.out.println(i);
+        }
+    }
+}
+
